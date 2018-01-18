cbc4b5c51c2f4b530db34ab2ccfaeb0f8957778b
ywCN <wangyouhao90@gmail.com>
2018-01-17 23:52:23 -0500

    separate classes

--- hw01.py
+++ hw01.py
@@ -1,12 +1,12 @@
 import unittest
 
 
-class TriangleClassification:
+class Triangle:
     def __init__(self):
         self.user_input = input('Please enter three sides separated by commas.')
         self.sides = self.parse_input()
         self.validate_input()
-        self.classify_triangle()
+        self.validate_triangle()
 
     def parse_input(self):
         elements = self.user_input.split(',')
@@ -16,22 +16,33 @@ class TriangleClassification:
             for element in elements:
                 parsed.append(float(element.strip()))
         except ValueError:
-            raise Exception('Please enter numbers.')
+            print('\'{}\' is not a valid input.'.format(self.user_input))
+            exit()
+            return False
 
         return sorted(parsed)
 
     def validate_input(self):
         if len(self.sides) != 3:
-            raise Exception('You must enter exactly 3 elements.')
+            print('Your triangle must have exactly 3 sides.')
+            return False
+        return True
 
     def validate_triangle(self):
         if self.sides[0] + self.sides[1] <= self.sides[2]:
-            raise Exception('Your triangle sides are not valid.')
+            print('Your triangle sides cannot build a triangle.')
+            return False
+        return True
+
+
+class TriangleClassification:
+    def __init__(self):
+        self.triangle = Triangle()
 
     def classify_triangle(self):
-        a = self.sides[0]
-        b = self.sides[1]
-        c = self.sides[2]
+        a = self.triangle.sides[0]
+        b = self.triangle.sides[1]
+        c = self.triangle.sides[2]
 
         triangle_types = ''
         if a == b and b == c:
@@ -43,12 +54,21 @@ class TriangleClassification:
             if round(a * a, 6) + round(b * b, 6) == round(c * c, 6):
                 triangle_types += ' and right'
 
+        return triangle_types
+
+    def run(self):
+        """
+        driver function
+        :return: None
+        """
+
+        triangle_types = self.classify_triangle()
         print('Your triangle is ' + triangle_types + '.')
 
 
 def main():
     demo = TriangleClassification()
-    demo.__init__()
+    demo.run()
 
 
 if __name__ == '__main__':
