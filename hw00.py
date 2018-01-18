0bbc51d12c72b04fc067d003f7492987fa5be016
ywCN <wangyouhao90@gmail.com>
2018-01-17 22:06:10 -0500

    hw00

---
+++ hw00.py
@@ -0,0 +1,15 @@
+class Homework00:
+
+    def __init__(self):
+        self.output = 'hello world'
+
+    def print_hello_world(self):
+        print(self.output)
+
+
+def main():
+    Homework00().print_hello_world()
+
+
+if __name__ == '__main__':
+    main()
