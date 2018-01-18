202760c8f9ad0e4bd0658d0b3546ce10ed0dbcd4
ywCN <wangyouhao90@gmail.com>
2018-01-18 00:20:10 -0500

    debugging: output type

--- hw01.py
+++ hw01.py
@@ -1,57 +1,57 @@
 import unittest
 
-
-class Triangle:
-    def __init__(self):
-        self.user_input = input('Please enter three sides separated by commas.')
-        self.sides = self.parse_input()
-        self.validate_input()
-        self.validate_triangle()
-
-    def parse_input(self):
-        elements = self.user_input.split(',')
-        parsed = []
-
-        try:
-            for element in elements:
-                parsed.append(float(element.strip()))
-        except ValueError:
-            print('\'{}\' is not a valid input.'.format(self.user_input))
-            exit()
-            return False
-
-        return sorted(parsed)
+class TriangleClassification:
+    def __init__(self, a=None, b=None, c=None):
+        self.sides = [a, b, c]
 
     def validate_input(self):
-        if len(self.sides) != 3:
-            print('Your triangle must have exactly 3 sides.')
+        if None in self.sides:
             return False
         return True
 
-    def validate_triangle(self):
+    def parse_input(self):
+        parsed = []
+        for element in self.sides:
+            parsed.append(float(element))
+        self.sides = sorted(parsed)
+
+    def validate_sides(self):
         if self.sides[0] + self.sides[1] <= self.sides[2]:
-            print('Your triangle sides cannot build a triangle.')
             return False
         return True
 
-
-class TriangleClassification:
-    def __init__(self):
-        self.triangle = Triangle()
+    @staticmethod
+    def check_right(a, b, c):
+        if round(a * a, 2) + round(b * b, 2) == round(c * c, 2):
+            return True
+        return False
 
     def classify_triangle(self):
-        a = self.triangle.sides[0]
-        b = self.triangle.sides[1]
-        c = self.triangle.sides[2]
+        if not self.validate_input():
+            return False
+
+        self.parse_input()
+        # print(self.sides)
+
+        if not self.validate_sides():
+            return False
+        print(self.sides)
+
+
+        a = self.sides[0]
+        b = self.sides[1]
+        c = self.sides[2]
 
         triangle_types = ''
         if a == b and b == c:
             triangle_types += 'equilateral'
         elif a == b or b == c:
             triangle_types += 'isosceles'
+            if self.check_right(a, b, c):
+                triangle_types += ' and right'
         else:
             triangle_types += 'scalene'
-            if round(a * a, 2) + round(b * b, 6) == round(c * c, 6):
+            if self.check_right(a, b, c):
                 triangle_types += ' and right'
 
         return triangle_types
@@ -63,24 +63,22 @@ class TriangleClassification:
         """
 
         triangle_types = self.classify_triangle()
-        print('Your triangle is ' + triangle_types + '.')
+
+        if triangle_types:
+            print('Your triangle is ' + triangle_types + '.')
 
 
-class TestTriangle(unittest.TestCase):
-    def test_parse_input(self):
-
-
-
-class TriangleClassification(unittest.TestCase):
+# class TestTriangleClassification(unittest.TestCase):
+#     def test
 
 
 
 def main():
-    demo = TriangleClassification()
+    demo = TriangleClassification(1, 2, 3)
     demo.run()
 
 
 if __name__ == '__main__':
     main()
-    unittest.main()
+    # unittest.main()
 
