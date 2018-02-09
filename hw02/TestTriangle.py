6bdf07692b44a553aac6cc84ecddbdfeebebe0b7
ywCN <wangyouhao90@gmail.com>
2018-02-08 21:35:18 -0500

    revise test cases

--- hw02/TestTriangle.py
+++ hw02/TestTriangle.py
@@ -7,28 +7,57 @@ from Triangle import classify_triangle
 
 
 class TestTriangles(unittest.TestCase):
-    # define multiple sets of tests as functions with names that begin
 
-    def testEquilateralTriangle(self):
-        self.assertEqual(classify_triangle(1, 1, 1), 'equilateral')
+    def testInvalidInput(self):
+        """
+        T1
+        2. Verify the Input
+        """
+        self.assertEqual(classify_triangle(2, 2, -3), 'not valid')
 
-    def testIsoscelesTriangle(self):
-        self.assertEqual(classify_triangle(2, 2, 3), 'isosceles')
+        self.assertEqual(classify_triangle('a', '2', 3), 'not valid')
 
-    def testScaleneTriangle(self):
-        self.assertEqual(classify_triangle(4, 5, 6), 'scalene')
+    def testInvalidTriangle(self):
+        """
+        T2
+        3. Verify that the sides form a legal triangle
+        :return:
+        """
+        self.assertEqual(classify_triangle(1, 2, 3), 'not valid')
 
     def testRightTriangle(self):
+        """
+        T3
+        4. Determine if the triangle is a right triangle
+        6. Display type of triangle
+        """
         self.assertEqual(classify_triangle(1, 1, 1.414213), 'isosceles and right')
 
         self.assertEqual(classify_triangle(3, 4, 5), 'scalene and right')
 
-    def testInvalidTriangle(self):
-        self.assertEqual(classify_triangle(1, 2, 3), 'not valid')
+    def testEquilateralTriangle(self):
+        """
+        T4
+        5.1 If the sides are all equal, then the triangle is “Equilateral”
+        6. Display type of triangle
+        """
+        self.assertEqual(classify_triangle(1, 1, 1), 'equilateral')
 
-        self.assertEqual(classify_triangle(2, 2, -3), 'not valid')
+    def testIsoscelesTriangle(self):
+        """
+        T5
+        5.2 Else if two side are equal, but not three, then the triangle is “Isosceles”
+        6. Display type of triangle
+        """
+        self.assertEqual(classify_triangle(2, 2, 3), 'isosceles')
 
-        self.assertEqual(classify_triangle('a', '2', 3), 'not valid')
+    def testScaleneTriangle(self):
+        """
+        T6
+        5.2 Else if two side are equal, but not three, then the triangle is “Isosceles”
+        6. Display type of triangle
+        """
+        self.assertEqual(classify_triangle(4, 5, 6), 'scalene')
 
 
 if __name__ == '__main__':
