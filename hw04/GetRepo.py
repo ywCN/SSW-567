daf096eb076fa0a1043d16a850ee791505a5520e
ywCN <wangyouhao90@gmail.com>
2018-02-09 13:04:15 -0500

    refactor

--- hw04/GetRepo.py
+++ hw04/GetRepo.py
@@ -7,8 +7,11 @@ def get_repo_info(user_name='ywang567'):
     user_url = 'https://api.github.com/users/{}/repos'.format(user_name)
     res = requests.get(user_url)
     repos = json.loads(res.text)
-    print(repos)  # can trigger API rate limit exceeded
     output.append('User: {}'.format(user_name))
+
+    if not validate_user_response(repos):
+        return 'unable to fetch repos from user'
+
     try:
         for repo in repos:
             repo_name = repo['name']
@@ -16,14 +19,20 @@ def get_repo_info(user_name='ywang567'):
             repo_info = requests.get(repo_url)
             repo_info_json = json.loads(repo_info.text)
             output.append('Repo: {} Number of commits: {}'.format(repo_name, len(repo_info_json)))
-            # print('Repo: {} Number of commits: {}'.format(repo_name, len(repo_info_json)))
-        for entry in output:
-            print(entry)
-        return output
     except TypeError:
-        return ''
+        return 'unable to fetch commits from repo'
+
+    return output
+
+
+def validate_user_response(repos):
+    try:
+        repos[0]['name']
+    except TypeError:
+        return False
+    return True
 
 
 if __name__ == '__main__':
-    get_repo_info()
-
+    for entry in get_repo_info():
+        print(entry)
